dda8b42aa04928491308c8fa1f2c2630d1f7ea4a
Naomi Aro <naomiaro@gmail.com>
2016-10-20 19:07:57 +0200

    passer à mailto au lieu de mail php

--- app/question/question.component.js
+++ app/question/question.component.js
@@ -1,128 +1,76 @@
 angular
-		.module('question')
-		.component(
-				'question',
-				{
-					templateUrl : 'app/question/question.template.html',
-					controller : [
-							'$http',
-							'$window',
-							'$rootScope',
-							'$scope',
-							'$filter',
-							function QuestionController($http, $window,
-									$rootScope, $scope, $filter) {
-								var self = this
-								var resultat = {
-									"NA" : 0,
-									"EA" : 0,
-									"NC" : 0,
-									"EC" : 0,
-									"EI" : 0,
-									"EN" : 0,
-									"ES" : 0,
-									"TS" : 0,
-									"ET" : 0,
-									"CN" : 0
-								};
+    .module('question')
+    .component(
+        'question', {
+            templateUrl: 'app/question/question.template.html',
+            controller: [
+                '$http',
+                '$window',
+                '$rootScope',
+                '$scope',
+                '$filter',
+                function QuestionController($http, $window,
+                    $rootScope, $scope, $filter) {
+                    var self = this
+                    self.msgClass = null;
+                    self.msg = null;
 
-								self.msgClass = null;
-								self.msg = null;
+                    $http.get('app/question/question.data.json')
+                        .then(function(response) {
+                            self.gps = response.data
+                        });
 
-								$http.get('app/question/question.data.json')
-										.then(function(response) {
-											self.gps = response.data
-										});
+                    self.valider = function valider() {
+                        if ($scope.formQuestion.$valid) {
+                            self.resultat = calculResultat(self.reponses);
+                            self.showresult = true;
+                        } else {
+                            self.msgClass = "error";
+                            self.msg = 'Tu dois r\351pondre \340 toutes les questions';
+                            $rootScope.Ui.turnOn('modalMsg');
+                        }
+                    }
 
-								self.valider = function valider() {
-									resultat = {
-										"NA" : 0,
-										"EA" : 0,
-										"NC" : 0,
-										"EC" : 0,
-										"EI" : 0,
-										"EN" : 0,
-										"ES" : 0,
-										"TS" : 0,
-										"ET" : 0,
-										"CN" : 0
-									};
-									if ($scope.formQuestion.$valid) {
-										angular
-												.forEach(
-														self.reponses,
-														function(value) {
-															value = angular
-																	.fromJson(value);
-															if (Object
-																	.keys(value) != "") {
-																key = Object
-																		.keys(value);
-																value = value[key];
-																resultat[key] = resultat[key]
-																		+ value;
-															}
-														});
-										self.resultat = resultat;
-										self.showresult = true;
-									} else {
-										self.msgClass = "error";
-										self.msg = 'Tu dois r\351pondre \340 toutes les questions';
-										$rootScope.Ui.turnOn('modalMsg');
-									}
-								}
+                    self.reset = function reset() {
+                        self.resultat = {}
+                        self.showresult = false;
+                        self.reponses = null;
+                    }
 
-								self.reset = function reset() {
-									self.resultat = resultat;
-									self.showresult = false;
-									self.reponses = null;
-								}
+                    self.mailto = function mailto() {
+                        var to = 'Mets ici le mail de ton animateur';
+                        var subject = 'Pour vivre un GPS ] Mets ici ton nom et ton pr\351nom';
+                        var body = resultatToString(self.resultat);
+                        $window.open("mailto:" + to + "?subject=" +
+                            subject + "&body=" + body,
+                            "_self");
+                    }
 
-								$scope.sendmail = function sendmail(user) {
-									var data = angular.toJson({
-										user : user,
-										gps : {
-											glossaire : self.gps.glossaires,
-											resultat : self.resultat
-										}
-									});
-									$http(
-											{
-												method : 'post',
-												url : 'app/question/question.sendmail.php?ts='+new Date().getTime(),
-												data : data,
-												cache: false
-											})
-											.then(
-													function successCallback(
-															response) {
-														if (response.data == 1) {
-															self.msgClass = 'success';
-															self.msg = 'Bravo! Tes r\351sultats sont bien envoy\351s \340 ton animateur. Au revoir '
-																	+ user.name
-																	+ '.';
-														} else {
-															self.msgClass = 'error';
-															self.msg = 'D\351sol\351, tes r\351sultats ne sont pas envoy\351s. Copie les vite sur un papier pour ne pas les perdre.';
-														}
-														$rootScope.Ui
-																.turnOff('modalSendmail');
-														$rootScope.Ui
-																.turnOn('modalMsg');
-													},
-													function errorCallback(
-															response) {
-														self.msgClass = 'error';
-														self.msg = "D\351sol\351, tes r\351sultats ne sont pas envoy\351 car tu n'as pas internet. Copie les vite sur un papier pour ne pas les perdre";
-														$rootScope.Ui
-																.turnOff('modalSendmail');
-														$rootScope.Ui
-																.turnOn('modalMsg');
+                    function calculResultat(reponse) {
+                        var resultat = {};
+                        angular.forEach(reponse, function(value) {
+                            value = angular.fromJson(value);
+                            if (Object.keys(value) != "") {
+                                key = Object.keys(value);
+                                value = value[key];
+                                resultat[key] = (resultat[key] ? resultat[key] : 0) + value;
+                            }
+                        });
+                        return resultat;
+                    }
 
-													});
+                    function resultatToString(resultat) {
+                        var ret = [];
+                        angular.forEach(resultat, function(value,
+                            key) {
+                            this.push(' ' +
+                                self.gps.glossaires[key] +
+                                ':' + value);
+                        }, ret);
+                        return ret.toString();
+                    }
 
-								}
+                }
+            ]
 
-							} ]
-
-				})
+        })
